439906d92f9cc4fda108b9c174e2c41d4b41db92
Hubert OG <hubert@orlikarnia.com>
2024-08-19 17:33:33 +0200

    Self-reload utility

--- src/fumbler/__init__.py
+++ src/fumbler/__init__.py
@@ -1,6 +1,8 @@
 import FreeCAD, FreeCADGui, Part
 import re
 import math
+import importlib
+import sys
 
 from .WrappedDocument import WrappedDocument
 
@@ -8,11 +10,14 @@ from .utils import TeethInset, TeethSide
 
 epsilon = 0.0001
 
-
-
 def create_wrapped_document(name):
   return WrappedDocument(name)
 
 
 def close_all():
   [FreeCAD.closeDocument(d) for d in FreeCAD.listDocuments()]
+
+def reload():
+  for name, module in list(sys.modules.items()):
+    if name.startswith("fumbler"):
+      importlib.reload(module)
--- src/fumbler/utils.py
+++ src/fumbler/utils.py
@@ -3,3 +3,4 @@ from enum import Enum
 TeethInset = Enum("TeethInset", ["Inset", "Outset"])
 TeethSide = Enum("TeethSide", ["Bottom", "Top"])
 
+CircleCap = Enum("CircleCap", ["None", "Flat", "Point", "RightPoint", "LeftPoint"])
