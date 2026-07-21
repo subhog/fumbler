42d4b15a66e1a0a7330b081e74579c25c91c11e1
Hubert OG <hubert@orlikarnia.com>
2026-07-22 01:19:17 +0200

    Cleanup

--- src/fumbler/WrappedDocument/make/_make_cube.py
+++ src/fumbler/WrappedDocument/make/_make_cube.py
@@ -17,14 +17,10 @@ def make_cube(
   cube.Width = y
   cube.Height = z
   part = WrappedPart(self, cube)
-  print("ANCHOR IS", anchor)
   if anchor.value == Anchor.Edge.value:
-    print("EDGE")
     part = part.move((-x / 2, 0, 0))
   elif anchor.value == Anchor.Face.value:
-    print("FACE")
     part = part.move((-x / 2, -y / 2, 0))
   elif anchor.value == Anchor.Volume.value:
-    print("VOLUME")
     part = part.move((-x / 2, -y / 2, -z / 2))
   return part
